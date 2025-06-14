f83ab9b4c6c123a265e9c13498d4564817c206f6
altomizawa <alyssontomizawa@hotmail.com>
2025-06-14 00:34:57 -0300

    fix: Reverse card order
    This commit reverses the order of the cards displayed on the homepage.
    
    - Updated the `Cards` component to display cards in reverse order using `cards.reverse()`. This ensures that the most recently added cards are displayed first.

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
     <>
       <Header user={user}/>
       <Profile user={user} />
-      <Cards user={user} cards={cards} />
+      <Cards user={user} cards={cards.reverse()} />
       <Footer />
     </>
   );
